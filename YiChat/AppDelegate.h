6076f00abdd17690581bfc5e98891abcbceb7ede
caijie <joseph7011@163.com>
2016-01-10 04:12:55 +0800

    Initial Commit

---
+++ YiChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YiChat
+//
+//  Created by Joseph on 16/1/10.
+//  Copyright © 2016年 Joseph. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YiChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YiChat
+//
+//  Created by Joseph on 16/1/10.
+//  Copyright © 2016年 Joseph. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
